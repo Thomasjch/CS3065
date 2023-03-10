b10c83f80ef2d57041fdaf08e2bb6dd40db93870
Thomasjch <71297513+Thomasjch@users.noreply.github.com>
2023-03-10 16:01:32 -0500

    Update task2.py

--- task2.py
+++ task2.py
@@ -6,25 +6,19 @@ import sklearn.model_selection
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.decomposition import PCA
 
+
 def train_test_split(  dataset: pd.DataFrame,
                        target_col: str, 
                        test_size: float,
                        stratify: bool,
                        random_state: int) -> tuple[pd.DataFrame,pd.DataFrame,pd.Series,pd.Series]:
-    
-    features = dataset.drop(target_col, axis=1)
-    target = dataset[target_col]
-
-    
-    if stratify:
-        train_features, test_features, train_targets, test_targets = sklearn.model_selection.train_test_split(
-            features, target, test_size=test_size, stratify=target, random_state=random_state)
-    else:
-        train_features, test_features, train_targets, test_targets = sklearn.model_selection.train_test_split(
-            features, target, test_size=test_size, random_state=random_state)
-
-    
-    return train_features, test_features, train_targets, test_targets
+    # TODO: Write the necessary code to split a dataframe into a Train and Test feature dataframe and a Train and Test 
+    # target series 
+    train_features = pd.DataFrame()
+    test_features = pd.DataFrame()
+    train_targets = pd.DataFrame()
+    test_targets = pd.DataFrame()
+    return train_features,test_features,train_targets,test_targets
 
 class PreprocessDataset:
     def __init__(self, 
@@ -35,70 +29,64 @@ class PreprocessDataset:
                  n_components:int,
                  feature_engineering_functions:dict
                  ):
-        self.train_features = train_features
-        self.test_features = test_features
-        self.one_hot_encode_cols = one_hot_encode_cols
-        self.min_max_scale_cols = min_max_scale_cols
-        self.n_components = n_components
-        self.feature_engineering_functions = feature_engineering_functions
-        
+        # TODO: Add any state variables you may need to make your functions work
+        return
+
     def one_hot_encode_columns_train(self) -> pd.DataFrame:
-        one_hot_encoded_dataset = pd.get_dummies(self.train_features, columns=self.one_hot_encode_cols)
+        # TODO: Write the necessary code to create a dataframe with the categorical column names in 
+        # the variable one_hot_encode_cols "one hot" encoded 
+        one_hot_encoded_dataset = pd.DataFrame()
+
         return one_hot_encoded_dataset
 
     def one_hot_encode_columns_test(self) -> pd.DataFrame:
-        one_hot_encoded_dataset = pd.get_dummies(self.test_features, columns=self.one_hot_encode_cols)
+        # TODO: Write the necessary code to create a dataframe with the categorical column names in 
+        # the variable one_hot_encode_cols "one hot" encoded 
+        one_hot_encoded_dataset = pd.DataFrame()
+
         return one_hot_encoded_dataset
 
     def min_max_scaled_columns_train(self) -> pd.DataFrame:
-        scaler = MinMaxScaler()
-        scaler.fit(self.train_features[self.min_max_scale_cols])
-        min_max_scaled_data = scaler.transform(self.train_features[self.min_max_scale_cols])
-        min_max_scaled_dataset = pd.DataFrame(min_max_scaled_data, columns=self.min_max_scale_cols)
+        # TODO: Write the necessary code to create a dataframe with the numerical column names in 
+        # the variable min_max_scale_cols scaled to the min and max of each column 
+        min_max_scaled_dataset = pd.DataFrame()
         return min_max_scaled_dataset
 
     def min_max_scaled_columns_test(self) -> pd.DataFrame:
-        scaler = MinMaxScaler()
-        scaler.fit(self.test_features[self.min_max_scale_cols])
-        min_max_scaled_data = scaler.transform(self.test_features[self.min_max_scale_cols])
-        min_max_scaled_dataset = pd.DataFrame(min_max_scaled_data, columns=self.min_max_scale_cols)
+        # TODO: Write the necessary code to create a dataframe with the numerical column names in 
+        # the variable min_max_scale_cols scaled to the min and max of each column 
+        min_max_scaled_dataset = pd.DataFrame()
         return min_max_scaled_dataset
     
     def pca_train(self) -> pd.DataFrame:
-        pca = PCA(n_components=self.n_components)
-        principal_components = pca.fit_transform(self.train_features)
-        pca_dataset = pd.DataFrame(data = principal_components,
-                                   columns = [f"component_{i+1}" for i in range(self.n_components)])
+        # TODO: use PCA to reduce the train_df to n_components principal components
+        # Name your new columns component_1, component_2 .. component_n 
+        pca_dataset = pd.DataFrame()
         return pca_dataset
 
     def pca_test(self) -> pd.DataFrame:
-        pca = PCA(n_components=self.n_components)
-        principal_components = pca.fit_transform(self.test_features)
-        pca_dataset = pd.DataFrame(data = principal_components,
-                                   columns = [f"component_{i+1}" for i in range(self.n_components)])
+        # TODO: use PCA to reduce the test_df to n_components principal components
+        # Name your new columns component_1, component_2 .. component_n 
+        pca_dataset = pd.DataFrame()
         return pca_dataset
 
     def feature_engineering_train(self) -> pd.DataFrame:
+        # TODO: Write the necessary code to create a dataframe with feature engineering functions applied 
+        # from the feature_engineering_functions dict (the dict format is {'feature_name':function,})
+        # each feature engineering function will take in type pd.DataFrame and return a pd.Series
         feature_engineered_dataset = pd.DataFrame()
-        for feature_name, feature_func in self.feature_engineering_functions.items():
-            feature_engineered_dataset[feature_name] = feature_func(self.train_features)
         return feature_engineered_dataset
 
     def feature_engineering_test(self) -> pd.DataFrame:
+        # TODO: Write the necessary code to create a dataframe with feature engineering functions applied 
+        # from the feature_engineering_functions dict (the dict format is {'feature_name':function,})
+        # each feature engineering function will take in type pd.DataFrame and return a pd.Series
         feature_engineered_dataset = pd.DataFrame()
-        for feature_name, feature_func in self.feature_engineering_functions.items():
-            feature_engineered_dataset[feature_name] = feature_func(self.test_features)
         return feature_engineered_dataset
 
     def preprocess(self) -> tuple[pd.DataFrame,pd.DataFrame]:
-        one_hot_encoded_train = self.one_hot_encode_columns_train()
-        one_hot_encoded_test = self.one_hot_encode_columns_test()
-        min_max_scaled_train = self.min_max_scaled_columns_train()
-        min_max_scaled_test = self.min_max_scaled_columns_test()
-        pca_train = self.pca_train()
-        pca_test = self.pca_test()
-        feature_engineered_train = self.feature_engineering_train()
-        feature_engineered_test = self.feature_engineering_test()
+        # TODO: Use the functions you wrote above to create train/test splits of the features and target with scaled and encoded values 
+        # for the columns specified in the init function
         train_features = pd.DataFrame()
         test_features = pd.DataFrame()
         return train_features,test_features
